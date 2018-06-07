75059555707846eb89836aeb447005e4caf3afc7
Elaine Anderson <eparie@bu.edu>
2018-06-07 20:01:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ end
 
 def kind
   if @length_1 == 0 || @length_2 == 0 || @length_3 == 0
-    TriangleError
+    raise TriangleError
   elsif @length_1 == @length_2 && @length_1 == @length_3
     :equilateral
   elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
