961d0561f9329e5badf693ad6d834578a5bfe5a5
Elaine Anderson <eparie@bu.edu>
2018-06-07 20:01:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,8 @@ def initialize(length_1, length_2, length_3)
 end
 
 def kind
-  if 
+  if @length_1 == 0 || @length_2 == 0 || @length_3 == 0
+    TriangleError
   elsif @length_1 == @length_2 && @length_1 == @length_3
     :equilateral
   elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
