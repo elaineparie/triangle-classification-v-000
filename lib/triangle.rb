7e72a2d523587bc3ca37aeb36ba99f037607555b
Elaine Anderson <eparie@bu.edu>
2018-06-07 20:01:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,8 @@ def initialize(length_1, length_2, length_3)
 end
 
 def kind
-  if @length_1 == @length_2 && @length_1 == @length_3
+  if 
+  elsif @length_1 == @length_2 && @length_1 == @length_3
     :equilateral
   elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
     :isosceles
